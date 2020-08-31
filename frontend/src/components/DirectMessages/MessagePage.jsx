123c3e4e51b476869b22e3dcd6da56983f2b2b22
csaidane <cs.saidane@gmail.com>
2020-08-31 22:40:31 +0000

    split point between 3 devs

--- frontend/src/components/DirectMessages/MessagePage.jsx
+++ frontend/src/components/DirectMessages/MessagePage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import MessageList from "./MessageList";
 import MessageTextBox from "./MessageTextBox";
 import MessageView from "./MessageView";
-import { makeStyles } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core";
+
 
 export default function MessagePage() {
+  // const classes = useStyles();
   return (
     <div className="dashboard-container" id="dashboard-container">
       <MessageList />
