66a9adceb0c62359b424910b967cf32eba449a38
katebatrakova <kateryna.batrakova@gmail.com>
2020-09-04 18:33:06 -0400

    Password and Edit btn show up on profile.

--- frontend/src/components/ProfilePage/BottomLayerProfileTutor.jsx
+++ frontend/src/components/ProfilePage/BottomLayerProfileTutor.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Grid } from "@material-ui/core";
+import { AppBar, Grid, Fab, Link } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import EditIcon from "@material-ui/icons/Edit";
+
 import "./BottomLayerProfileTutor.scss";
 
 function TabPanel(props) {
@@ -73,7 +75,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function BottomLayerProfileTutor(props) {
   const { bio, education, rate_per_hour } = props.user.tutor;
-  const { name, email, post_code, province, city, street } = props.user.user;
+  const {
+    name,
+    password,
+    email,
+    post_code,
+    province,
+    city,
+    street,
+  } = props.user.user;
 
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
@@ -83,7 +93,23 @@ export default function BottomLayerProfileTutor(props) {
   };
 
   return (
-    <div className={classes.root}>
+    <Grid className={classes.root}>
+      <Grid
+        item
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Fab
+          href="/editprofile"
+          color="secondary"
+          aria-label="edit"
+          style={{ marginBottom: "3%" }}
+        >
+          <EditIcon />
+        </Fab>
+      </Grid>
       <AppBar position="static">
         <Tabs
           variant="fullWidth"
@@ -114,6 +140,10 @@ export default function BottomLayerProfileTutor(props) {
               <strong>Email: </strong> {email}
             </p>
             <p className={classes.padding}>
+              <strong>Password: </strong>
+              {password}
+            </p>
+            <p className={classes.padding}>
               <strong>Education:</strong> {education}
             </p>
             <p className={classes.padding}>
@@ -141,6 +171,6 @@ export default function BottomLayerProfileTutor(props) {
         </Grid>
       </TabPanel>
       <Grid item lg={12} id="footer"></Grid>
-    </div>
+    </Grid>
   );
 }
